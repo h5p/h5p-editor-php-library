2be95c992f786ebe85687c675c5a8fa30756e73e
Svein-Tore Griff With <with@amendor.com>
2013-07-16 14:42:01 +0200

    OPPG-465: Fix a bug in core so that dimensions aren't changed when they've already been set

--- scripts/h5peditor-dimensions.js
+++ scripts/h5peditor-dimensions.js
@@ -26,8 +26,8 @@ ns.Dimensions = function (parent, field, params, setValue) {
 
   // Find image field to get default size from.
   H5PEditor.followField(parent, field['default'], function (file, index) {
-    // Make sure we don't set size if we have one in params.
-    if (index === undefined || params === undefined) {
+    // Make sure we don't set size if we have one in the default params.
+    if (params.width === undefined) {
       that.setSize(file);
     }
   });
