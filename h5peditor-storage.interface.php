24bf69130523425c0b70112de2bb61568f349deb
Svein-Tore Griff With <s.t.g.with@gmail.com>
2013-11-14 23:32:55 +0100

    Merging

--- h5peditor-storage.interface.php
+++ h5peditor-storage.interface.php
@@ -1,5 +1,6 @@
-<?php
-// TODO: Document this (What is provided in params (and how), and what is expected in return (how))
+<?php 
+//FIXME: Document this (What is provided in params (and how), and what is
+//expected in return (how))
 
 interface H5peditorStorage {
   public function getSemantics($machine_name, $major_version, $minor_version);
