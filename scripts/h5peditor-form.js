d3c5281251b94ff870f2b6dc5c067d1cfa2ee003
Frode Petterson <frode.andre.petterson@gmail.com>
2014-08-27 10:50:58 +0200

    Made common fields collapsible, renamed and added description.

--- scripts/h5peditor-form.js
+++ scripts/h5peditor-form.js
@@ -5,12 +5,18 @@ var ns = H5PEditor;
  * Construct a form from library semantics.
  */
 ns.Form = function () {
+  var self = this;
+  
   this.params = {};
   this.passReadies = false;
   this.commonFields = {};
-  this.$form = ns.$('<div class="h5peditor-form"><div class="tree"></div><div class="common"><div class="h5peditor-label">' + ns.t('core', 'commonFields') + '</div></div></div>');
-  this.$common = this.$form.children('.common');
+  this.$form = ns.$('<div class="h5peditor-form"><div class="tree"></div><div class="common collapsed"><div class="h5peditor-label"><span class="icon"></span>' + ns.t('core', 'commonFields') + '</div><div class="fields"><p class="desc">' + ns.t('core', 'commonFieldsDescription') + '</p></div></div></div>');
+  this.$common = this.$form.find('.common > .fields');
   this.library = '';
+  
+  this.$common.prev().click(function () {
+    self.$common.parent().toggleClass('collapsed');
+  });
 };
 
 /**
