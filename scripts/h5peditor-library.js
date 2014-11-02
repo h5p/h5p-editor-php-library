36d47b0300066aeebf6b559768422cdddcb10429
Svein-Tore Griff With <s.t.g.with@gmail.com>
2014-11-02 14:12:42 +0100

    Pick changes that was added to 7.x-1.x instead of the library

--- scripts/h5peditor-library.js
+++ scripts/h5peditor-library.js
@@ -63,7 +63,7 @@ ns.Library.prototype.appendTo = function ($wrapper) {
     var options = ns.createOption('-', '-');
     for (var i = 0; i < data.length; i++) {
       var library = data[i];
-      if (library.title !== undefined) {
+      if (library.title !== undefined && (library.restricted === undefined || !library.restricted)) {
         options += ns.createOption(library.uberName, library.title, library.uberName === that.params.library);
       }
     }
