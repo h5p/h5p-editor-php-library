32b9efc65d264a2423387687b50e14a2f47e28f7
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-02-18 10:03:01 +0100

    Refactor and comment

--- scripts/h5peditor-library-list-cache.js
+++ scripts/h5peditor-library-list-cache.js
@@ -1,69 +1,92 @@
 /** @namespace H5PEditor */
 var H5PEditor = H5PEditor || {};
 
-H5PEditor.LibraryListCache = function() {
-  H5P.EventDispatcher.call(this);
-  this.libraryCache = {};
-  this.librariesComingIn = {};
-  this.librariesMissing = {};
-  this.que = [];
+/**
+ * The library list cache
+ * 
+ * @type Object
+ */
+var llc = H5PEditor.LibraryListCache = {
+  libraryCache: {},
+  librariesComingIn: {},
+  librariesMissing: {},
+  que: []
 };
 
-H5PEditor.LibraryListCache.prototype = Object.create(H5P.EventDispatcher.prototype);
-H5PEditor.LibraryListCache.prototype.constructor = H5PEditor.LibraryListCache;
-
-H5PEditor.LibraryListCache.prototype.getLibraries = function(libraries, handler, scope) {
+/**
+ * Get data for a list of libraries
+ * 
+ * @param {Array} libraries - list of libraries to load info for (uber names)
+ * @param {Function} handler - Callback when list of libraries is loaded
+ * @param {Function} thisArg - Context for the callback function
+ */
+llc.getLibraries = function(libraries, handler, thisArg) {
   var cachedLibraries = [];
   var status = 'hasAll';
-  var self = this;
   for (var i = 0; i < libraries.length; i++) {
-    if (libraries[i] in this.libraryCache) {
+    if (libraries[i] in llc.libraryCache) {
       // Libraries that are missing on the server are set to null...
-      if (this.libraryCache[libraries[i]] !== null) {
-        cachedLibraries.push(this.libraryCache[libraries[i]]);
+      if (llc.libraryCache[libraries[i]] !== null) {
+        cachedLibraries.push(llc.libraryCache[libraries[i]]);
       }
     }
-    else if (libraries[i] in this.librariesComingIn) {
+    else if (libraries[i] in llc.librariesComingIn) {
       if (status === 'hasAll') {
         status = 'onTheWay';
       }
     }
     else {
       status = 'requestThem';
-      this.librariesComingIn[libraries[i]] = true;
+      llc.librariesComingIn[libraries[i]] = true;
     }
   }
   switch (status) {
     case 'hasAll':
-      handler.call(scope, cachedLibraries);
+      handler.call(thisArg, cachedLibraries);
       break;
   case 'onTheWay':
-    this.que.push({libraries: libraries, handler: handler, scope: scope});
+    llc.que.push({libraries: libraries, handler: handler, thisArg: thisArg});
     break;
   case 'requestThem':
-    ns.$.post(ns.getAjaxUrl('libraries'), {libraries: libraries}, function (data) {
-      self.setLibraries(data, libraries);
-      handler.call(scope, data);
-      self.runQue();
-    });
+    var ajaxParams = {
+      type: "POST",
+      url: H5PEditor.getAjaxUrl('libraries'),
+      success: function(data) {
+        llc.setLibraries(data, libraries);
+        handler.call(thisArg, data);
+        llc.runQue();
+      },
+      data: {'libraries': libraries},
+      dataType: "json"
+    };
+    H5PEditor.$.ajax(ajaxParams);
     break;
   }
 };
 
-H5PEditor.LibraryListCache.prototype.runQue = function() {
-  var l = this.que.length
+/**
+ * Call all qued handlers
+ */
+llc.runQue = function() {
+  var l = llc.que.length;
   for (var i = 0; i < l; i++) {
-    var handlerObject = this.que.shift();
-    this.getLibraries(handlerObject.libraries, handlerObject.handler, handlerObject.scope);
+    var handlerObject = llc.que.shift();
+    llc.getLibraries(handlerObject.libraries, handlerObject.handler, handlerObject.thisArg);
   }
 };
 
-H5PEditor.LibraryListCache.prototype.setLibraries = function(libraries, requestedLibraries) {
+/**
+ * We've got new libraries from the server, save them
+ * 
+ * @param {Array} libraries - Libraries with info from server
+ * @param {Array} requestedLibraries - List of what libraries we requested
+ */
+llc.setLibraries = function(libraries, requestedLibraries) {
   var reqLibraries = requestedLibraries.slice();
   for (var i = 0; i < libraries.length; i++) {
-    this.libraryCache[libraries[i].uberName] = libraries[i];
-    if (libraries[i].uberName in this.librariesComingIn) {
-      delete this.librariesComingIn[libraries[i].uberName];
+    llc.libraryCache[libraries[i].uberName] = libraries[i];
+    if (libraries[i].uberName in llc.librariesComingIn) {
+      delete llc.librariesComingIn[libraries[i].uberName];
     }
     var index = reqLibraries.indexOf(libraries[i].uberName);
     if (index > -1) {
@@ -71,11 +94,9 @@ H5PEditor.LibraryListCache.prototype.setLibraries = function(libraries, requeste
     }
   }
   for (var i = 0; i < reqLibraries.length; i++) {
-    this.libraryCache[reqLibraries[i]] = null;
-    if (reqLibraries[i] in this.librariesComingIn) {
-      delete this.librariesComingIn[libraries[i]];
+    llc.libraryCache[reqLibraries[i]] = null;
+    if (reqLibraries[i] in llc.librariesComingIn) {
+      delete llc.librariesComingIn[libraries[i]];
     }
   }
 };
-
-H5PEditor.LibraryListCache = new H5PEditor.LibraryListCache(); 
