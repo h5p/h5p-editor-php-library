859164120d8fc7a6df813ed9ea5657981ad26276
Frode Petterson <frode.andre.petterson@gmail.com>
2015-03-20 10:29:33 +0100

    Use correct path in CSS files as well.

--- h5peditor.class.php
+++ h5peditor.class.php
@@ -330,7 +330,7 @@ class H5peditor {
     // Stylesheets
     if (!empty($files['styles'])) {
       foreach ($files['styles'] as $css) {
-        H5peditor::buildCssPath(NULL, $this->basePath . dirname($css->path) . '/');
+        H5peditor::buildCssPath(NULL, $this->h5p->url . dirname($css->path) . '/');
         $libraryData->css[$this->h5p->url . $css->path . $css->version] = preg_replace_callback('/url\([\'"]?(?![a-z]+:|\/+)([^\'")]+)[\'"]?\)/i', 'H5peditor::buildCssPath', file_get_contents($this->h5p->path . $css->path));
       }
     }
