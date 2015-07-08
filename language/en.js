39cbb6990042e6fe5c1d36148301ad0de5a4684a
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-07-08 14:08:45 +0200

    Better warning before content type change

--- language/en.js
+++ language/en.js
@@ -20,7 +20,7 @@ H5PEditor.language.core = {
   addEntity: 'Add :entity',
   tooLong: 'Field value is too long, should contain :max letters or less.',
   invalidFormat: 'Field value contains an invalid format or characters that are forbidden.',
-  confirmChangeLibrary: 'Are you sure you wish to change library?',
+  confirmChangeLibrary: 'By doing this you will loose all work done with the current content type. Are you sure you wish to change content type?',
   moreLibraries: 'Look for <a href="http://h5p.org/content-types-and-applications" target="_blank">more content types</a> on h5p.org',
   commonFields: 'Settings and texts',
   commonFieldsDescription: 'Here you can edit settings or translate texts used in this content.',
--- language/nn.js
+++ language/nn.js
@@ -20,7 +20,7 @@ H5PEditor.language.core = {
   addEntity: 'Legg til :entity',
   tooLong: 'Feltets verdi er for lang, den må vere på :max tegn eller mindre.',
   invalidFormat: 'Feltets verdi er på eit ugyldig format eller bruker ulovlege tegn.',
-  confirmChangeLibrary: 'Er du sikker på at du ønskjer å byte bibliotek?',
+  confirmChangeLibrary: 'Gjør du dette mister du alt arbeid gjort med nåværande innhaldstype. Er du sikker på at du ønskjer å byte innhaldstype?',
   moreLibraries: 'Sjå etter <a href="http://h5p.org/content-types-and-applications" target="_blank">fleir innhaldstypar</a> på h5p.org',
   commonFields: 'Innstillinger og tekstar',
   commonFieldsDescription: 'Her kan du redigere innstillinger eller oversette tekstar som brukast i dette innhaldet.',
