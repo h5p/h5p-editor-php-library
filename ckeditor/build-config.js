68c0b5a14dcf398d54331e468942d95a25d7be03
Paal Joergensen <pal.jorgensen@gmail.com>
2015-09-09 13:08:49 +0200

    Added alignment to ckeditor (including upgraded ckeditor) [HPN-115]

--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/af1f152fd9a4529fa9566d0478c0210f
+ * (2) http://ckeditor.com/builder/d11c5298ce0a8359647f3feee4926f41
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/af1f152fd9a4529fa9566d0478c0210f
+ * (3) http://ckeditor.com/builder/download/d11c5298ce0a8359647f3feee4926f41
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -42,6 +42,7 @@ var CKBUILDER_CONFIG = {
 		'.jscsrc',
 		'.jshintignore',
 		'.jshintrc',
+		'less',
 		'.mailmap',
 		'node_modules',
 		'package.json',
@@ -64,6 +65,7 @@ var CKBUILDER_CONFIG = {
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'indentlist' : 1,
+		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
 		'magicline' : 1,
--- ckeditor/config.js
+++ ckeditor/config.js
@@ -27,10 +27,10 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-//	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+//      config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
 
 	// Dialog windows are also simplified.
-//	config.removeDialogTabs = 'link:advanced';
+//      config.removeDialogTabs = 'link:advanced';
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
 	// Se the most common block elements.
