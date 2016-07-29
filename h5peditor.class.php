c525d15b265984a0757c582fe9f04285af98518b
Frode Petterson <frode.andre.petterson@gmail.com>
2016-07-29 14:14:24 +0200

    Fixed copy and paste issue
    HFJ-2099

--- h5peditor.class.php
+++ h5peditor.class.php
@@ -33,7 +33,7 @@ class H5peditor {
     'scripts/h5peditor-none.js',
     'ckeditor/ckeditor.js',
   );
-  private $h5p, $storage, $files_directory, $basePath, $relativePathRegExp;
+  private $h5p, $storage, $files_directory, $basePath;
 
   /**
    * Constructor for the core editor library.
@@ -42,15 +42,13 @@ class H5peditor {
    * @param mixed $storage Instance of h5peditor storage.
    * @param string $basePath Url path to prefix assets with.
    * @param string $filesDir H5P files directory.
-   * @param string $editorFilesDir Optional custom editor files directory outside h5p files directory.
    */
-  function __construct($h5p, $storage, $basePath, $filesDir, $editorFilesDir = NULL, $relativePathRegExp = '/^(\.\.\/){1,2}(.*content\/)?(\d+|editor)\/(.+)$/') {
+  function __construct($h5p, $storage, $basePath, $filesDir, $editorFilesDir = NULL) {
     $this->h5p = $h5p;
     $this->storage = $storage;
     $this->basePath = $basePath;
     $this->contentFilesDir = $filesDir . DIRECTORY_SEPARATOR . 'content';
     $this->editorFilesDir = ($editorFilesDir === NULL ? $filesDir . DIRECTORY_SEPARATOR . 'editor' : $editorFilesDir);
-    $this->relativePathRegExp = $relativePathRegExp;
   }
 
   /**
@@ -266,14 +264,14 @@ class H5peditor {
 
     // File could be copied from another content folder.
     $matches = array();
-    if (preg_match($this->relativePathRegExp, $params->path, $matches)) {
+    if (preg_match($this->h5p->relativePathRegExp, $params->path, $matches)) {
       // Create copy of file
-      $source = $this->content_directory . '../' . $matches[3] . '/' . $matches[4];
-      $destination = $this->content_directory . $matches[4];
+      $source = $this->content_directory . $matches[1] . $matches[4] . '/' . $matches[5];
+      $destination = $this->content_directory . $matches[5];
       if (file_exists($source) && !file_exists($destination)) {
         copy($source, $destination);
       }
-      $params->path = $matches[4];
+      $params->path = $matches[5];
     }
     else {
       // Check if tmp file
