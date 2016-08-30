d8a6020610df478c485f7468d87c4c264d6c2734
Håkon Ellingsen <hakonweb@gmail.com>
2016-08-30 08:35:00 +0200

    HVP-76: Always listen to input `change` event

--- scripts/h5peditor-list-editor.js
+++ scripts/h5peditor-list-editor.js
@@ -252,18 +252,18 @@ H5PEditor.ListEditor = (function ($) {
         var childWidget = ns.getWidgetName(item.children[0].field);
 
         if (childWidget === 'html') {
+          var itemLabel = item.field.label;
+
           // Change label to reflect content of listgroup
           if (item.params.text && item.params.text !== undefined) {
-            var itemLabel = item.field.label;
-
             setListgroupTitle(itemLabel, $(item.params.text).text())
-
-            item.children[0].$input.change(function (e) {
-              // TODO: Get text from CKEditor. The `innerText` of target
-              //       doesn't actually change at first.
-              setListgroupTitle(itemLabel, e.target.innerText);
-            });
           }
+
+          item.children[0].$input.change(function (e) {
+            // TODO: Get text from CKEditor. The `innerText` of target
+            //       doesn't actually change at first.
+            setListgroupTitle(itemLabel, e.target.innerText);
+          });
         }
       }
 
