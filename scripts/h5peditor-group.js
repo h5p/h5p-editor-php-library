ebe742ea6beabe10d767e0b89d8f9dafea43dfc2
Tom Arild Jakobsen <tajakobsen@gmail.com>
2016-11-29 16:07:40 +0100

    Handle isSubContent better. Relates to: #HFP-285

--- scripts/h5peditor-group.js
+++ scripts/h5peditor-group.js
@@ -49,7 +49,7 @@ ns.Group = function (parent, field, params, setValue) {
   }
 
   if (this.field.optional === true) {
-    // If this field is optional, make sure child fields are aswell
+    // If this field is optional, make sure child fields are as well
     for (var j = 0; j < this.field.fields.length; j++) {
       this.field.fields[j].optional = true;
     }
@@ -106,7 +106,7 @@ ns.Group.prototype.appendTo = function ($wrapper) {
     appendTo: this.$group
   });
 
-  if (this.field.fields.length === 1) {
+  if (this.hasSingleChild() && !this.isSubContent()) {
     $content.addClass('h5peditor-single');
     this.children = [];
     var field = this.field.fields[0];
@@ -122,9 +122,9 @@ ns.Group.prototype.appendTo = function ($wrapper) {
       this.setValue(this.field, this.params);
     }
 
-    var params = this.initSubContent(this.params);
+    this.params = this.initSubContent(this.params);
 
-    ns.processSemanticsChunk(this.field.fields, params, $content, this);
+    ns.processSemanticsChunk(this.field.fields, this.params, $content, this);
   }
 
   // Set summary
@@ -138,19 +138,42 @@ ns.Group.prototype.appendTo = function ($wrapper) {
 };
 
 /**
+ * Return whether this group is Sub Content
+ *
+ * @private
+ * @return {boolean}
+ */
+ns.Group.prototype.hasSingleChild = function () {
+  return this.field.fields.length === 1;
+};
+
+/**
  * Add generated 'subContentId' attribute, if group is "sub content (library-like embedded structure)"
  *
  * @param {object} params
+ *
+ * @private
  * @return {object}
  */
 ns.Group.prototype.initSubContent = function (params) {
   // If group contains library-like sub content that needs UUIDs
-  if(this.field.isSubContent === true){
-    params['subContentId'] = params['subContentId'] || this.guid();
+  if(this.isSubContent()){
+    params['subContentId'] = params['subContentId'] || H5P.createUUID();
   }
 
   return params;
 };
+
+/**
+ * Return whether this group is Sub Content
+ *
+ * @private
+ * @return {boolean}
+ */
+ns.Group.prototype.isSubContent = function () {
+  return this.field.isSubContent === true;
+};
+
 /**
  * Toggle expand/collapse for the given group.
  */
@@ -199,7 +222,7 @@ ns.Group.prototype.findSummary = function () {
     if (child.field === undefined) {
       continue;
     }
-    var params = this.field.fields.length === 1 ? this.params : this.params[child.field.name];
+    var params = (that.hasSingleChild() && !that.isSubContent()) ? this.params : this.params[child.field.name];
     var widget = ns.getWidgetName(child.field);
 
     if (widget === 'text') {
@@ -208,7 +231,7 @@ ns.Group.prototype.findSummary = function () {
       }
 
       child.$input.change(function () {
-        var params = that.field.fields.length === 1 ? that.params : that.params[child.field.name];
+        var params = (that.hasSingleChild() && !that.isSubContent()) ? that.params : that.params[child.field.name];
         if (params !== undefined && params !== '') {
           that.setSummary(params.replace(/(<([^>]+)>)/ig, ""));
         }
@@ -301,21 +324,6 @@ ns.Group.prototype.remove = function () {
     ns.removeChildren(this.children);
     this.$group.remove();
   }
-};
-
-/**
- * Generate new UUID
- *
- * @return {string}
- */
-ns.Group.prototype.guid = function () {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
-  }
-
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 };
 
 // Tell the editor what widget we are.
