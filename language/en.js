57fdf4415331650a9dca23803fc075abc20f8c55
Timothy Lim <timothylim23@gmail.com>
2017-04-06 11:33:08 +0200

    HFP-894 Add reload button label

--- language/en.js
+++ language/en.js
@@ -137,5 +137,6 @@ H5PEditor.language.core = {
   imageLightBoxProgress: ':num of :total',
   nextImage: 'Next image',
   previousImage: 'Previous image',
-  screenhots: 'Screenshots'
+  screenhots: 'Screenshots',
+  reloadButtonLabel: 'Reload'
 };
