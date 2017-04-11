92f4069b1f4ce6324191923466c1dba4e703ad1d
Timothy Lim <timothylim23@gmail.com>
2017-04-11 15:35:15 +0200

    HFP-920 Update all language files with licenseCannotHoldLiable

--- language/pl.js
+++ language/pl.js
@@ -130,6 +130,7 @@ H5PEditor.language.core = {
   licenseCanDistribute: 'Can distribute',
   licenseCanSublicense: 'Can sublicense',
   licenseCanHoldLiable: 'Can hold liable',
+  licenseCannotHoldLiable: 'Cannot hold liable',
   licenseMustIncludeCopyright: 'Must include copyright',
   licenseMustIncludeLicense: 'Must include license',
   licenseFetchDetailsFailed: 'Failed fetching license details',
