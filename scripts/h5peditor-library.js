e52e87f2039ad3ecccb40994c6971325c0bd4e33
thomasmars <thomasmars87@gmail.com>
2018-10-31 13:33:06 +0100

    HFP-2360 Add "paste & replace" text when paste will replace content

--- scripts/h5peditor-library.js
+++ scripts/h5peditor-library.js
@@ -370,6 +370,8 @@ ns.Library.prototype.loadLibrary = function (libraryName, preserveParams) {
 
     this.$libraryWrapper.attr('class', 'libwrap');
     this.$copyButton.toggleClass('disabled', true);
+    this.$pasteButton.text(ns.t('core', 'pasteButton'));
+    this.$pasteButton.attr('title', ns.t('core', 'pasteFromClipboard'));
     return;
   }
 
@@ -413,6 +415,8 @@ ns.Library.prototype.loadLibrary = function (libraryName, preserveParams) {
     ns.processSemanticsChunk(semantics, that.params.params, that.$libraryWrapper, that);
     if (window.localStorage) {
       that.$copyButton.toggleClass('disabled', false);
+      that.$pasteButton.text(ns.t('core', 'pasteAndReplaceButton'));
+      that.$pasteButton.attr('title', ns.t('core', 'pasteAndReplaceFromClipboard'));
     }
 
     if (that.metadataForm && metadataSettings.disableExtraTitleField) {
