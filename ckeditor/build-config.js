3aed32da433d6ec9265c7196dd4e3da8e032069b
Frode Petterson <frode.andre.petterson@gmail.com>
2019-06-25 15:42:02 +0200

    Update CKEditor to 4.11.4

--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -1,6 +1,6 @@
 ﻿/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
 /**
@@ -10,13 +10,13 @@
  * If you would like to build CKEditor online again
  * (for example to upgrade), visit one the following links:
  *
- * (1) http://ckeditor.com/builder
+ * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/a03d6ba5620152a5a7776bda9efdf4b4
+ * (2) https://ckeditor.com/cke4/builder/d5a8e6ba8b97eef6fe69e57d3dd6d8b7
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/a03d6ba5620152a5a7776bda9efdf4b4
+ * (3) https://ckeditor.com/cke4/builder/download/d5a8e6ba8b97eef6fe69e57d3dd6d8b7
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -38,6 +38,7 @@ var CKBUILDER_CONFIG = {
 		'.jshintignore',
 		'.jshintrc',
 		'.mailmap',
+		'.npm',
 		'.travis.yml',
 		'README.md',
 		'bender-err.log',
