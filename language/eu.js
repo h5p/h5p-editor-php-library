6b65d488208cd79042feda3f9daf2f3abe9cff9d
Iñigo Zendegi <izendegi@mondragon.edu>
2019-08-29 10:12:30 +0200

    Update eu.js

--- language/eu.js
+++ language/eu.js
@@ -212,7 +212,7 @@ H5PEditor.language.core = {
   notAllTextsChanged: 'Ez dira testu guztiak aldatu, :language hizkuntzaren sostengua ez da osoa.',
   contributeTranslations: ':language hizkuntzako itzulpena osatu nahi baduzu <a href=":url" target="_new">contributing translations to H5P</a> estekan nola egin ikasi dezakezu',
   unknownLibrary: 'Tamalez, aukeratutako \'%lib\' eduki-mota ez dago sistema honetan instalatuta.',
-  proceedButtonLabel: 'Proceed to save',
-  enterFullscreenButtonLabel: 'Enter fullscreen',
-  exitFullscreenButtonLabel: 'Exit fullscreen'
+  proceedButtonLabel: 'Jarraitu eta gorde',
+  enterFullscreenButtonLabel: 'Erabili pantaila osoko modura',
+  exitFullscreenButtonLabel: 'Irten pantaila osoko modutik'
 };
