9dca350ce82501e48b1870ce75faebc46eda99d4
Oliver Tacke <o.tacke@posteo.de>
2022-01-13 19:46:49 +0100

    Tweak translations for video version description

--- language/de.js
+++ language/de.js
@@ -165,7 +165,7 @@ H5PEditor.language.core = {
   videoQualityDefaultLabel: 'Videoqualität :index',
 =======
   videoVersion: 'Video version label',
-  videoVersionDescription: 'This label helps the user identify the current version of the video. Could e.g. be "1080p" or "Mobile" if your versions represent different video qualities or "Normal" or "sign language" if your versions represent different levels of accessibility.',
+  videoVersionDescription: 'This label helps the user identify the current version of the video. Could e.g. be "1080p" or "Mobile" if your versions represent different video qualities or "Without sign language" or "With sign language" if your versions represent different levels of accessibility.',
   videoVersionDefaultLabel: 'Version :index',
 >>>>>>> Add support for multiple YouTube videos
   noContentTypesAvailable: 'Keine Inhaltstypen verfügbar',
