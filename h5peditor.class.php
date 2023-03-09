f9ee61c39e7fb961efc399dec3da98d6a4d09b00
Meirza <115134930+meirzamoodle@users.noreply.github.com>
2023-03-09 21:48:25 +0700

    Fixed dynamic properties deprecation in PHP 8.2 (#169)

--- h5peditor.class.php
+++ h5peditor.class.php
@@ -51,7 +51,7 @@ class H5peditor {
     'ckeditor/ckeditor.js',
   );
   private $h5p, $storage;
-  public $ajax, $ajaxInterface;
+  public $ajax, $ajaxInterface, $content;
 
   /**
    * Constructor for the core editor library.
