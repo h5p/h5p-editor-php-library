6d76d656d795e7e043c98d7792e2133c6c7cb704
devland <5208532+devland@users.noreply.github.com>
2024-01-23 12:22:30 +0100

    Ji 5682 align cropper popup (#201)
    * JI-5684 fix cropper rotation
    
    * JI-5684 update crop limit logic
    
    * JI-5684 fix scroll into view
    
    * JI-5684 remove scroll into view
    
    * JI-5682 align cropper popup
    
    * JI-5682 update cropper align to work in cors iframe
    
    * JI-5682 revert align cropper popup
    
    * JI-5682 add new alignPopup function for crop tool
    
    * JI-5682 update function description  with new param
    
    ---------
    
    Co-authored-by: makmentins <maris.akmentins@h5p.group>

--- libs/cropper.js
+++ libs/cropper.js
@@ -294,14 +294,19 @@ function Cropper(options) {
    *
    * @param {Blob} blob Raw image data.
    */
-  const handleBlob = (blob) => {
-    const url = URL.createObjectURL(blob);
-    this.image = new Image();
-    this.image.onload = () => {
-      URL.revokeObjectURL(url);
-      this.loadImage();
+  const handleBlob = (callback) => {
+    return (blob) => {
+      const url = URL.createObjectURL(blob);
+      this.image = new Image();
+      this.image.onload = () => {
+        URL.revokeObjectURL(url);
+        this.loadImage();
+        if (callback) {
+          callback();
+        }
+      }
+      this.image.src = url;
     }
-    this.image.src = url;
   }
 
   /**
@@ -332,7 +337,7 @@ function Cropper(options) {
    * Crops image based on selector size & position relative to image location within canvas.
    * The output is generated from the mirror canvas based on scaled selector dimensions.
    */
-  this.crop = () => {
+  this.crop = (callback) => {
     let painted = this.fit(this.image);
     let selectedX = this.selector.offsetLeft - this.margins.left;
     let selectedY = this.selector.offsetTop - this.margins.top;
@@ -361,7 +366,7 @@ function Cropper(options) {
     this.mirror.width = width;
     this.mirror.height = height;
     this.mirrorContext.drawImage(this.image, selectedX, selectedY, selectedWidth, selectedHeight, 0, 0, width, height);
-    this.mirror.toBlob(handleBlob, 'image/png', 1);
+    this.mirror.toBlob(handleBlob(callback), 'image/png', 1);
   }
 
   /**
@@ -369,7 +374,7 @@ function Cropper(options) {
    *
    * @param {integer} rotation Clockwise rotation for positive numbers. Counterclockwise for negative numbers.
    */
-  this.rotate = (rotation) => {
+  this.rotate = (rotation, callback) => {
     rotation %= 4;
     if (rotation % 2) {
       this.mirror.width = this.image.height;
@@ -383,7 +388,7 @@ function Cropper(options) {
     this.mirrorContext.rotate(90 * rotation * Math.PI / 180);
     this.mirrorContext.translate(-this.image.width / 2, -this.image.height / 2);
     this.mirrorContext.drawImage(this.image, 0, 0);
-    this.mirror.toBlob(handleBlob, 'image/png', 1);
+    this.mirror.toBlob(handleBlob(callback), 'image/png', 1);
   }
 
   /**
