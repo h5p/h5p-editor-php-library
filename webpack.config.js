b28c38aa75e097eb96a54186c09c09074422a56a
William Almnes <william.almnes@gmail.com>
2024-04-12 14:24:12 +0200

    JI-5452 remove compass and use webpack to build css (#228)
    Co-authored-by: William Almnes <william.almnes@h5p.group>

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "styles/css"),
+  },
+  entry: {
+    index: [path.join(path.resolve(__dirname, 'styles'), "scss", "application.scss")]
+  } ,
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: './fonts/[name][ext]',
+        },
+      },
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "application.css"
+  })]
+};
