0dbdf3ebead5a99c21b122f6694a33ec116eef07
devland <adevland@gmail.com>
2024-04-16 15:12:18 +0200

    JI-4001 build zebra_datepicker from npm

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,40 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, "styles/css"),
-  },
-  entry: {
-    index: [path.join(path.resolve(__dirname, 'styles'), "scss", "application.scss")]
-  } ,
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)$/,
-        type: 'asset/resource',
-        generator: {
-            filename: './fonts/[name][ext]',
+module.exports = [
+  {
+    output: {
+      path: path.resolve(__dirname, "styles/css"),
+    },
+    entry: {
+      index: [path.join(path.resolve(__dirname, 'styles'), "scss", "application.scss")]
+    } ,
+    mode: "production",
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
         },
-      },
-    ]
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)$/,
+          type: 'asset/resource',
+          generator: {
+              filename: './fonts/[name][ext]',
+          },
+        },
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({
+      filename: "application.css"
+    })]
   },
-  plugins: [new MiniCssExtractPlugin({
-    filename: "application.css"
-  })]
-};
+  {
+    entry: './zebra_datepicker.js',
+    output: {
+      path: path.resolve(__dirname, './libs'),
+      filename: 'zebra_datepicker.min.js'
+    },
+    mode: process.env.NODE_ENV || 'production',
+  }
+];
---
+++ zebra_datepicker.js
@@ -0,0 +1,2 @@
+require('jquery');
+require('zebra_datepicker');
