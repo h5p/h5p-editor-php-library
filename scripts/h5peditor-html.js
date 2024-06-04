2972eef48a611a0d69ec56e1da63430376376163
makmentins <maris.akmentins@h5p.group>
2024-06-04 14:57:51 +0200

    JI-5408 merge JI-5152 into wordpress, keep h5p-hub-client build files that of wordpress, remove unnecessary leftover ckeditor4 files

--- scripts/h5peditor-html.js
+++ scripts/h5peditor-html.js
@@ -36,35 +36,46 @@ ns.Html.prototype.inButtons = function (button) {
 };
 
 ns.Html.prototype.getCKEditorConfig = function () {
-  const basicstyles = [];
-  const plugins = ['Essentials', 'Paragraph'];
-  const alignments = { options: ["left", "center", "right"] };
-  const paragraph = [];
-  const formats = [];
-  const inserts = [];
-  const toolbar = [];
+  const config = {
+    updateSourceElementOnDestroy: true,
+    plugins: ['Essentials', 'Paragraph'],
+    alignment: { options: ["left", "center", "right"] },
+    toolbar: [],
+  };
 
   // Basic styles
+  const basicstyles = [];
+  const basicstylesPlugins = [];
   if (this.inTags("strong") || this.inTags("b")) {
     basicstyles.push('bold');
-    plugins.push('Bold');
+    basicstylesPlugins.push('Bold');
     // Might make "strong" duplicated in the tag lists. Which doesn't really
     // matter. Note: CKeditor will only make strongs.
     this.tags.push("strong");
   }
   if (this.inTags("em") || this.inTags("i")) {
+    // Use <em> elements for italic text instead of CKE's default <i>
+    // Has to be a plugin to work
+    const ItalicAsEmPlugin = function (editor) {
+      editor.conversion.for('downcast').attributeToElement({
+        model: 'italic',
+        view: 'em',
+        converterPriority: 'high',
+      });
+    }
+
     basicstyles.push('italic');
-    plugins.push('Italic');
+    basicstylesPlugins.push('Italic', ItalicAsEmPlugin);
     this.tags.push("i");
   }
   if (this.inTags("u")) {
     basicstyles.push('underline');
-    plugins.push('Underline');
+    basicstylesPlugins.push('Underline');
     this.tags.push("u");
   }
   if (this.inTags("strike") || this.inTags("del") || this.inTags("s")) {
     basicstyles.push('strikethrough');
-    plugins.push('Strikethrough');
+    basicstylesPlugins.push('Strikethrough');
     // Might make "strike" or "del" or both duplicated in the tag lists. Which
     // again doesn't really matter.
     this.tags.push("strike");
@@ -73,25 +84,28 @@ ns.Html.prototype.getCKEditorConfig = function () {
   }
   if (this.inTags("sub")) {
     basicstyles.push("subscript");
-    plugins.push('Subscript');
+    basicstylesPlugins.push('Subscript');
   }
   if (this.inTags("sup")) {
     basicstyles.push("superscript");
-    plugins.push('Superscript');
+    basicstylesPlugins.push('Superscript');
   }
   if (basicstyles.length > 0) {
     basicstyles.push('|', 'removeFormat');
-    plugins.push('RemoveFormat');
-    toolbar.push(...basicstyles);
+    basicstylesPlugins.push('RemoveFormat');
+    config.plugins.push(...basicstylesPlugins);
+    config.toolbar.push(...basicstyles);
   }
 
   // Alignment is added to all wysiwygs
-  plugins.push('Alignment');
-  toolbar.push('|', 'alignment');
+  config.plugins.push('Alignment');
+  config.toolbar.push('|', 'alignment');
 
   // Paragraph styles
+  const paragraph = [];
+  const paragraphPlugins = [];
   if (this.inTags("ul") || this.inTags("ol")) {
-    plugins.push('List');
+    paragraphPlugins.push('List');
   }
   if (this.inTags("ul")) {
     paragraph.push("bulletedList");
@@ -103,78 +117,121 @@ ns.Html.prototype.getCKEditorConfig = function () {
   }
   if (this.inTags("blockquote")) {
     paragraph.push("blockquote");
-    plugins.push('BlockQuote');
+    paragraphPlugins.push('BlockQuote');
   }
   if (this.inButtons('language')) {
     this.tags.push('span');
     paragraph.push('textPartLanguage');
-    plugins.push('TextPartLanguage');
+    paragraphPlugins.push('TextPartLanguage');
   }
   if (paragraph.length > 0) {
-    toolbar.push(...paragraph);
+    config.plugins.push(...paragraphPlugins);
+    config.toolbar.push(...paragraph);
   }
 
   // Links.
   if (this.inTags("a")) {
     const items = ["link"];
-    plugins.push('Link'); // TODO: add plugin 'AutoLink' once available in h5p-ckeditor repo
-    toolbar.push("|", ...items);
+    config.plugins.push('Link', 'AutoLink');
+    config.toolbar.push("|", ...items);
+    config.link = {
+      // Automatically add target="_blank" and rel="noopener noreferrer" to all external links.
+      addTargetToExternalLinks: true,
+      // Automatically add protocol if not present
+      defaultProtocol: 'http://',
+    }
   }
 
   // Inserts
+  const inserts = [];
+  const insertsPlugins = [];
   if (this.inTags('img')) {
     // TODO: Include toolbar functionality to insert and edit images
     // For now, we just include the plugin to prevent data loss
-    plugins.push('Image');
-  }
-  // Include table plugins to avoid errors when creating the editor
-  plugins.push('Table', 'TableToolbar');
-  if (this.inTags("table")) {
-    inserts.push("insertTable");
-    ns.$.merge(this.tags, ["tr", "td", "th", "colgroup", "thead", "tbody", "tfoot"]);
+    insertsPlugins.push('Image');
   }
   if (this.inTags("hr")) {
     inserts.push("horizontalLine");
-    plugins.push('HorizontalLine');
+    insertsPlugins.push('HorizontalLine');
   }
   if (this.inTags('code')) {
     if (this.inButtons('inlineCode')) {
       inserts.push('code');
-      plugins.push('Code');
+      insertsPlugins.push('Code');
     }
     if (this.inTags('pre') && this.inButtons('codeSnippet')) {
       inserts.push('codeBlock');
-      plugins.push('CodeBlock');
+      insertsPlugins.push('CodeBlock');
     }
   }
   if (inserts.length > 0) {
-    toolbar.push("|", ...inserts);
+    config.toolbar.push("|", ...inserts);
+  }
+  if (insertsPlugins.length > 0) {
+    config.plugins.push(...insertsPlugins);
   }
 
-  const config = {
-    updateSourceElementOnDestroy: true,
-    plugins: plugins,
-    alignment: alignments,
-    toolbar: toolbar,
-    table: {
-      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+  if (this.inTags("table")) {
+    config.toolbar.push("insertTable");
+    config.plugins.push(
+      'Table',
+      'TableToolbar',
+      'TableProperties',
+      'TableCellProperties',
+      'TableColumnResize',
+      'TableCaption'
+    );
+    config.table = {
+      contentToolbar: [
+        'toggleTableCaption',
+        'tableColumn',
+        'tableRow',
+        'mergeTableCells',
+        'tableProperties',
+        'tableCellProperties'
+      ],
+      tableProperties: {
+        defaultProperties: {
+          borderStyle: 'underline',
+          borderWidth: '0.083em',
+          borderColor: '#494949',
+          padding: '0',
+          alignment: 'left'
+        }
+      },
+      tableCellProperties: {
+        defaultProperties: {
+          borderStyle: 'underline',
+          borderWidth: '0.083em',
+          borderColor: '#494949',
+          padding: '1px'
+        }
+      }
     }
-  };
+    ns.$.merge(this.tags, ["tr", "td", "th", "colgroup", "col", "thead", "tbody", "tfoot", "figure", "figcaption"]);
+  }
 
   // Add dropdown to toolbar if formatters in tags (h1, h2, etc).
+  const formats = [];
   for (let index = 1; index < 7; index++) {
     if (this.inTags('h' + index)) {
       formats.push({ model: 'heading' + index, view: 'h' + index, title: 'Heading ' + index, class: 'ck-heading_heading' + index });
     }
   }
+
+  if (this.inTags('pre')) {
+    formats.push({ model: 'formatted', view: 'pre', title: 'Formatted', class: 'ck-heading_formatted' });
+  }
+
   // if (this.inTags("address")) formats.push("address"); // TODO: potential data loss
   if (formats.length > 0 || this.inTags('p') || this.inTags('div')) {
     // If the formats are shown, always have a paragraph
     formats.push({ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' });
     this.tags.push("p");
-    config['heading'] = {options: formats};
-    config['plugins'].push('Heading');
-    config['toolbar'].push('heading');
+    this.field.enterMode = "p";
+    config.heading = {options: formats};
+    config.plugins.push('Heading');
+    config.toolbar.push('heading');
   }
 
   if (this.field.font !== undefined) {
@@ -199,39 +256,61 @@ ns.Html.prototype.getCKEditorConfig = function () {
     };
 
     // Font family chooser
-    // TODO: not used in any official h5p content types
-    // TODO: Data loss risk for open source types that use it
-    // if (this.field.font.family) {
-    //   styles.push('fontFamily');
-    //   config['plugins'].push('FontFamily');
+    if (this.field.font.family) {
+      styles.push('fontFamily');
+      config.plugins.push('FontFamily');
 
-    //   if (this.field.font.family instanceof Array) {
-    //     // Use specified families
-    //     setValues(this.field.font.family, 'fontFamily');
-    //   }
-    // }
+      let fontFamilies = [
+        'default',
+        'Arial, Helvetica, sans-serif',
+        'Comic Sans MS, Cursive, sans-serif',
+        'Courier New, Courier, monospace',
+        'Georgia, serif',
+        'Lucida Sans Unicode, Lucida Grande, sans-serif',
+        'Tahoma, Geneva, sans-serif',
+        'Times New Roman, Times, serif',
+        'Trebuchet MS, Helvetica, sans-serif',
+        'Verdana, Geneva, sans-serif'
+      ]
+
+      // If custom fonts are set, use those
+      if (this.field.font.family instanceof Array) {
+        fontFamilies = ['default', ...this.field.font.family.map(font => (
+          font.label + ', ' + font.css
+        ))];
+      }
+
+      setValues(fontFamilies, 'fontFamily');
+      config.fontFamily.supportAllValues = true;
+    }
 
     // Font size chooser
     if (this.field.font.size) {
       styles.push('fontSize');
-      config['plugins'].push('FontSize');
+      config.plugins.push('FontSize');
+
+      let fontSizes = [];
+      const convertToEm = (percent) => parseFloat(percent) / 100 + 'em';
 
       if (this.field.font.size instanceof Array) {
         // Use specified sizes
-        // TODO: likely not backwards compatible as is
-        setValues(this.field.font.size, 'fontSize');
+        fontSizes = this.field.font.size.map(size => ({
+          title: size.label,
+          model: convertToEm(size.css)
+        }));
+      } else {
+        // Standard font sizes that are available
+        fontSizes = [
+          'Default', '50%', '56.25%', '62.5%', '68.75%', '75%', '87.5%',
+          '100%', '112.5%', '125%', '137.5%', '150%', '162.5%', '175%',
+          '225%', '300%', '450%'
+        ].map(percent => ({
+          title: percent,
+          model: percent === 'Default' ? '1em' : convertToEm(percent)
+        }));
       }
-      else {
-        const fontSizes = [];
 
-        // Standard font sizes that are available.
-        const defaultAvailable = [8, 9, 10, 11, 12, 14, 'default', 18, 20, 22, 24, 26, 28, 36, 48, 72];
-
-        for (let i = 0; i < defaultAvailable.length; i++) {
-          fontSizes.push(defaultAvailable[i]);
-        }
-        setValues(fontSizes, 'fontSize');
-      }
+      setValues(fontSizes, 'fontSize');
     }
 
     /**
@@ -261,29 +340,29 @@ ns.Html.prototype.getCKEditorConfig = function () {
     // Text color chooser
     if (this.field.font.color) {
       colors.push('fontColor');
-      config['plugins'].push('FontColor');
+      config.plugins.push('FontColor');
 
       if (this.field.font.color instanceof Array) {
-        config['fontColor'] = { colors: getColors(this.field.font.color) };
+        config.fontColor = { colors: getColors(this.field.font.color) };
       }
     }
 
     // Text background color chooser
     if (this.field.font.background) {
       colors.push('fontBackgroundColor');
-      config['plugins'].push('FontBackgroundColor');
+      config.plugins.push('FontBackgroundColor');
 
       if (this.field.font.background instanceof Array) {
-        config['fontBackgroundColor'] = { colors: getColors(this.field.font.color) };
+        config.fontBackgroundColor = { colors: getColors(this.field.font.color) };
       }
     }
 
     // Add the text styling options
     if (styles.length) {
-      toolbar.push(...styles);
+      config.toolbar.push(...styles);
     }
     if (colors.length) {
-      toolbar.push(...colors);
+      config.toolbar.push(...colors);
     }
   }
 
@@ -292,6 +371,17 @@ ns.Html.prototype.getCKEditorConfig = function () {
   }
   else {
     this.tags.push('div');
+
+    // Without this, empty divs get deleted on init of cke
+    config.plugins.push('GeneralHtmlSupport');
+    config.htmlSupport = {
+      allow: [{
+        name: 'div',
+        attributes: true,
+        classes: true,
+        styles: true
+      }]
+    };
   }
 
   return config;
@@ -332,13 +422,16 @@ ns.Html.prototype.appendTo = function ($wrapper) {
     // Remove existing CK instance.
     ns.Html.removeWysiwyg();
 
+    that.inputWidth = that.$input[0].getBoundingClientRect().width;
     ns.Html.current = that;
 
     ClassicEditor
       .create(this, that.ckEditorConfig)
       .then(editor => {
         const getEditorHeight = () => {
-          const { innerHeight, innerWidth } = window;
+          // Use the dimensions of the H5P iframe, not the editor iframe
+          const { innerHeight, innerWidth } = window.parent;
+
           let ratio = 0.5;
 
           switch (true) {
@@ -361,15 +454,30 @@ ns.Html.prototype.appendTo = function ($wrapper) {
               break;
           }
 
-          return ratio * innerHeight * 0.85;
+          return (ratio * innerHeight * 0.85) + 'px';
         }
 
-        // Use <em> elements for italic text instead of <i>
-        editor.conversion.for('downcast').attributeToElement( {
-          model: 'italic',
-          view: 'em',
-          converterPriority: 'high'
-        });
+        that.ckeditor = editor;
+        const editable = editor.ui.view.editable;
+        editorElement = editable.element;
+        editor.ui.view.element.style.maxWidth = that.inputWidth + 'px';
+        editorElement.style.maxHeight = getEditorHeight();
+
+        // Readjust toolbar's grouped item dropdown panel,
+        // since it can overflow the parent iframe element by using default positioning
+        const dropdownPanel = editor.ui.view.toolbar._behavior.groupedItemsDropdown;
+        dropdownPanel.panelPosition = 'auto';
+
+        // Disable sticky toolbar, since it has problem within iframes
+        editor.ui.view.stickyPanel.unbind('isActive');
+        editor.ui.view.stickyPanel.isActive = false;
+
+        // Remove overflow protection on startup
+        let initialData = editor.getData();
+        if (initialData.includes('table-overflow-protection')) {
+          initialData = initialData.replace(/<div class=\"table-overflow-protection\">.*<\/div>/, '');
+          editor.setData(initialData);
+        }
 
         // Mimic old enter_mode behaviour if not specifically set to 'p'
         if (that.field.enterMode !== 'p') {
@@ -381,20 +489,15 @@ ns.Html.prototype.appendTo = function ($wrapper) {
           });
         }
 
-        that.ckeditor = editor;
-        const editable = editor.ui.view.editable;
-        editorElement = editable.element;
-        editorElement.style.maxHeight = getEditorHeight() + 'px';
-
-        editable.on('change', (event) => {
-          editorElement = event.source.element;
-          editorElement.style.maxHeight = getEditorHeight() + 'px';
+        editor.ui.on('update', () => {
+          editorElement.style.maxHeight = getEditorHeight();
         });
 
         editor.editing.view.focus();
 
         editor.on('focus', function () {
           blurFired = false;
+          editorElement.style.maxHeight = getEditorHeight();
         });
 
         editor.once('destroy', function () {
@@ -411,6 +514,14 @@ ns.Html.prototype.appendTo = function ($wrapper) {
           const value = editor.getData();
           if (that.$placeholder.length !== 0 && (value === undefined || value.length === 0) && (that.value === undefined || that.value.length === 0)) {
             that.$placeholder.appendTo(that.$item.find('.ckeditor'));
+          }
+
+          // Since validate() is not always run,
+          // make sure tabe overflow protection is added always when editor is destroyed
+          if (value.includes('table') && !value.includes('table-overflow-protection')) {
+            that.value = value + '<div class="table-overflow-protection"></div>';
+            that.setValue(that.field, that.value);
+            that.$input.html(that.value).change();
           }
         });
 
@@ -455,6 +566,10 @@ ns.Html.prototype.createHtml = function () {
   else if (this.field.placeholder !== undefined) {
     input += '<span class="h5peditor-ckeditor-placeholder">' + this.field.placeholder + '</span>';
   }
+  // Add overflow protection if table
+  if (this.field.tags.includes('table') && !input.includes('table-overflow-protection')) {
+    input += '<div class="table-overflow-protection"></div>';
+  }
   input += '</div>';
 
   return ns.createFieldMarkup(this.field, ns.createImportantDescription(this.field.important) + input, id);
@@ -489,8 +604,8 @@ ns.Html.prototype.validate = function () {
   // Check if we have any text at all.
   if (!this.field.optional && !textValue.length) {
     // We can accept empty text, if there's an image instead.
-    if (! (this.inTags("img") && $value.find('img').length > 0)) {
-      this.$errors.append(ns.createError(ns.t('core', 'requiredProperty', {':property': ns.t('core', 'textField')})));
+    if (!(this.inTags("img") && $value.find('img').length > 0)) {
+      this.$errors.append(ns.createError(ns.t('core', 'requiredProperty', { ':property': ns.t('core', 'textField') })));
     }
   }
 
@@ -498,10 +613,17 @@ ns.Html.prototype.validate = function () {
   // the tag's content.  So if we get an unallowed container, the contents
   // will remain, without the container.
   $value.find('*').each(function () {
-    if (! that.inTags(this.tagName)) {
+    if (!that.inTags(this.tagName)) {
       ns.$(this).replaceWith(ns.$(this).contents());
     }
   });
+
+  // Add overflow protection if chance of aligned tables
+  if(that.inTags('table') && !value.includes('table-overflow-protection')) {
+    this.$input.append('<div class="table-overflow-protection"></div>');
+    $value.append('<div class="table-overflow-protection"></div>');
+  }
+
   value = $value.html();
 
   // Display errors and bail if set.
