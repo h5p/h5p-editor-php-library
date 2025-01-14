e65f289a40c72982c898a3d8df0d7eec95608a5f
Simen Bergo <bergo.simen@gmail.com>
2025-01-14 11:59:17 +0100

    JI-6929 Add echo360 to description as video source

--- language/et.js
+++ language/et.js
@@ -65,7 +65,7 @@ H5PEditor.language.core = {
   insertLinkTitle: 'URL',
   recordAudioTitle: 'Record Audio',
   addVideoDescription:
-    'H5P supports all external video sources formatted as mp4, webm or ogv, like Vimeo Pro, and has support for YouTube and Panopto links.',
+    'H5P supports all external video sources formatted as mp4, webm or ogv, like Vimeo Pro, and has support for YouTube, Panopto, and Echo360 links.',
   unsupportedVideoSource: "Unsupported video source",
   unsupportedAudioSource: "Unsupported audio source",
   insert: 'Aseta',
